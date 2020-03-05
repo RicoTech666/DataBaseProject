eac532ccf9d106ce14c6ab50555786e37e23da27
RicoTech666 <zhoujiachenbe@gmail.com>
2020-03-05 12:53:49 +0800

    [Jiachen] feat query controller finished

--- src/controllers/QueryController.java
+++ src/controllers/QueryController.java
@@ -19,31 +19,31 @@ public class QueryController {
                 return "SELECT t1.name,t2.subject,t3.score FROM student as t1\n" +
                         "INNER JOIN subject as t2\n" +
                         "INNER JOIN score as t3\n" +
-                        "on t1.id = t3.student_id AND t2.id = t3.subject_id AND t1.name =\"" + sc.next()+"\"";
+                        "on t1.id = t3.student_id AND t2.id = t3.subject_id AND t1.name =\"" + sc.next() + "\"";
             case "1.1.3":
                 System.out.println("请输入具体要查询的老师姓名：");
                 return "SELECT t2.teacher,t1.name,t2.subject,t3.score FROM student as t1\n" +
                         "INNER JOIN subject as t2\n" +
                         "INNER JOIN score as t3 \n" +
-                        "on t1.id = t3.student_id AND t2.id = t3.subject_id AND t2.teacher =\"" + sc.next()+"\"";
+                        "on t1.id = t3.student_id AND t2.id = t3.subject_id AND t2.teacher =\"" + sc.next() + "\"";
             case "1.1.4":
                 System.out.println("请输入具体要查询的课程名称：");
                 return "SELECT t2.subject,t1.name,t3.score FROM student as t1\n" +
                         "INNER JOIN subject as t2\n" +
                         "INNER JOIN score as t3 \n" +
-                        "on t1.id = t3.student_id AND t2.id = t3.subject_id AND t2.subject =\"" + sc.next()+"\"";
+                        "on t1.id = t3.student_id AND t2.id = t3.subject_id AND t2.subject =\"" + sc.next() + "\"";
             case "1.2.1":
                 return "SELECT * FROM subject";
             case "1.2.2":
                 return "SELECT subject FROM subject";
             case "1.2.3":
                 System.out.println("请输入具体要查询的老师姓名：");
-                return "SELECT subject，teacher FROM subject WHERE teacher =\"" + sc.next()+"\"";
+                return "SELECT subject，teacher FROM subject WHERE teacher =\"" + sc.next() + "\"";
             case "1.3.1":
                 return "SELECT teacher,subject FROM subject";
             case "1.3.2":
                 System.out.println("请输入要查询的老师姓名：");
-                return "SELECT teacher,subject FROM subject WHERE teacher =\"" + sc.next()+"\"";
+                return "SELECT teacher,subject FROM subject WHERE teacher =\"" + sc.next() + "\"";
             case "2.1":
                 System.out.println("请输入学生信息(例如：学号：1001，姓名： 小明, 年龄： 18, 性别： 男)：");
 
@@ -55,7 +55,8 @@ public class QueryController {
 
                 System.out.println("添加学生[ " + studentName + studentId + " ]成功！");
                 return "INSERT INTO student (id,name,age,sex)\n" +
-                        "VALUES(" + studentId + ",\"" + studentName + "\"," + studentAge + ",\"" + studentGender + "\")";
+                        "VALUES(" + studentId + ",\"" + studentName + "\"," + studentAge + ",\"" + studentGender +
+                        "\")";
             case "2.2":
                 System.out.println("请输入课程信息(例如：科目编号：1001，科目： 语文, 代课老师： 李老师)：");
                 String subjectInfo = sc.next();
@@ -87,7 +88,7 @@ public class QueryController {
                 return "INSERT INTO score (student_id,subject_id,score)\n" +
                         "SELECT t1.id,t2.id," + scoreVal + "FROM student AS t1\n" +
                         "INNER JOIN subject AS t2\n" +
-                        "ON t1.name = \"" + scoreStu + "\"AND t2.subject = \"" + scoreSubj+"\"";
+                        "ON t1.name = \"" + scoreStu + "\"AND t2.subject = \"" + scoreSubj + "\"";
             case "3.1":
                 System.out.println("请输入学生信息(例如：学号：1001，姓名： 小明, 年龄： 18, 性别： 男)：");
                 String stuInfoAlter = sc.next();
@@ -127,10 +128,50 @@ public class QueryController {
                         "(SELECT t1.id AS stu_id, t2.id AS subj_id FROM student as t1\n" +
                         "INNER JOIN subject AS t2\n" +
                         "INNER JOIN score AS t3\n" +
-                        "ON t1.id = t3.student_id AND t2.id = t3.subject_id AND t1.name =\"" + scoreSubjAlter +"\"\n"+
+                        "ON t1.id = t3.student_id AND t2.id = t3.subject_id AND t1.name =\"" + scoreSubjAlter + "\"\n" +
                         "AND t2.subject=\"" + scoreStuAlter + "\")AS score_info\n" +
                         "SET score.score = " + scoreValAlter + " WHERE score.student_id = score_info.stu_id\n" +
                         "AND score.subject_id = score_info.subj_id";
+            case "4.1":
+                System.out.println("删除学生之后，该学生信息将不能恢复，是否要继续删除？\n" +
+                        "1. 是\n" +
+                        "2. 否");
+                if ("1".equals(sc.next())) {
+                    System.out.println("请输入要删除的学生信息：(例：学号：1001)");
+                    String stuIdDelete = sc.next().split("：")[1].trim();
+
+                    System.out.println("删除学生 " + stuIdDelete + " 成功！");
+                    return "DELETE FROM student where id=" + stuIdDelete;
+                } else {
+                    return "";
+                }
+            case "4.2":
+                System.out.println("删除课程之后，该课程信息将不能恢复，是否要继续删除？\n" +
+                        "1. 是\n" +
+                        "2. 否");
+                if ("1".equals(sc.next())) {
+                    System.out.println("请输入要删除的课程信息：(例：课程号：1001)");
+                    String subjIdDelete = sc.next().split("：")[1].trim();
+
+                    System.out.println("删除课程 " + subjIdDelete + " 成功！");
+                    return "DELETE FROM subject where id=" + subjIdDelete;
+                } else {
+                    return "";
+                }
+            case "4.3":
+                System.out.println("删除老师之后，该老师信息将不能恢复，是否要继续删除？\n" +
+                        "1. 是\n" +
+                        "2. 否");
+                if ("1".equals(sc.next())) {
+                    System.out.println("请输入要删除的老师信息：(例：老师编号： 1001)");
+                    String teacherIdDelete = sc.next().split("：")[1].trim();
+
+                    System.out.println("删除老师 " + teacherIdDelete + " 成功！");
+                    return "DELETE FROM teacher where id=" + teacherIdDelete;
+                } else {
+                    return "";
+                }
+
             default:
                 System.out.println("对不起，您的输入有误，无此对应查找项目");
                 return "";
